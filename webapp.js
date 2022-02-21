960ebe3ffa73d261c07378c87dfd881ccb16c9f8
franciscoo11 <78971729+franciscoo11@users.noreply.github.com>
2022-02-21 14:36:47 -0300

    add lib express-upload

--- webapp.js
+++ webapp.js
@@ -3,6 +3,7 @@ const aplicacion = express()
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const flash = require('express-flash')
+const fileUpload = require('express-fileupload')
 
 const rutasMiddleware = require('./routes/middleware')
 const rutasPublics = require('./routes/publics')
@@ -14,6 +15,7 @@ aplicacion.set("view engine", "ejs")
 aplicacion.use(session({ secret: 'token-muy-secreto', resave: true, saveUninitialized: true }));
 aplicacion.use(flash())
 aplicacion.use(express.static('public'))
+aplicacion.use(fileUpload)
 
 aplicacion.use(rutasMiddleware)
 aplicacion.use(rutasPublics)
