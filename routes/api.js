e8c9b14ad75cee38be78c1bd624fd4995190867f
Francisco Giudicatti <franciscogiudicatti11@gmail.com>
2022-02-21 22:47:54 -0300

    update api endpoints

--- routes/api.js
+++ routes/api.js
@@ -10,17 +10,27 @@ var pool = mysql.createPool({
     database: 'blog_viajes'
 })
 
-router.get('api/v1/publicaciones?busqueda', function (req, res) {
+router.get('api/v1/publicaciones', function (req, res) {
     pool.getConnection(function (err, connection) {
-        const query = ` 
+        const busqueda_api = ( req.query.busqueda ) ? req.query.busqueda : ""
+        if (busqueda_api != ""){
+            const query = ` 
             SELECT * FROM publicaciones WHERE
-            titulo LIKE '%${req.query.busqueda}%' OR
-            resumen LIKE '%${req.query.busqueda}%' OR
-            contenido LIKE '%${req.query.busqueda}%'
-        `
+            titulo LIKE '%${busqueda_api}%' OR
+            resumen LIKE '%${busqueda_api}%' OR
+            contenido LIKE '%${busqueda_api}%'
+            ORDER BY fecha_hora DESC
+            `
+        }
         connection.query(query, function (error, filas, campos) {
-            res.status(200)
-            res.json({ data: filas })
+           if (!error) {
+                res.status(200)
+                res.json({ data: filas })
+            }
+            else {
+                res.status(404)
+                res.send({errors: 'No se encontraron resultados de la busqueda.'})
+            }
         })
         connection.release()
     })
@@ -89,31 +99,65 @@ router.get('api/v1/autores/:id', function (req, res) {
     })
 })
 
-router.post('/api/v1/autores', function (req, res) {
+router.post('/api/v1/autores/', function (req, res) {
     pool.getConnection(function (err, connection) {
-        const query = `
-        INSERT 
-        INTO autores 
-        (pseudonimo, email, contrasena) 
-        VALUES
-        (${connection.escape(req.body.pseudonimo)},${connection.escape(req.body.email)},${connection.escape(req.body.contrasena)}
+        const email = req.body.email.toLowerCase().trim()
+        const pseudonimo = req.body.pseudonimo.trim()
+        const contrasena = req.body.contrasena
+        const consultaEmail = `
+            SELECT *
+            FROM autores
+            WHERE email = ${connection.escape(email)}
         `
-        connection.query(query, function (error, filas, campos) {
-            const nuevoId = filas.insertId
-            const queryConsulta = ` SELECT * FROM autores WHERE id = ${connection.escape(nuevoId)} `
+        connection.query(consultaEmail, (error, filas, campos) => {
+            if (filas.length > 0) {
+                res.status(400)
+                res.send({ errors: 'El email o el pseoudonimo ya se encuentran registrados.' })
+            }
+            else {
+                const consultaPseudonimo = `
+                    SELECT *
+                    FROM autores
+                    WHERE pseudonimo = ${connection.escape(pseudonimo)}
+                `
+                connection.query(consultaPseudonimo, (error, filas, campos) => {
+                    if (filas.length > 0) {
+                        res.status(400)
+                        res.send({ errors: 'El email o el pseoudonimo ya se encuentran registrados.' })
+                    }
+                    else {
+                        const query = `
+                        INSERT 
+                        INTO autores 
+                        (pseudonimo, email, contrasena) 
+                        VALUES
+                        (${connection.escape(email)},${connection.escape(pseudonimo)},${connection.escape(contrasena)}
+                        `
+                        connection.query(query, function (error, filas, campos) {
+                            const nuevoId = filas.insertId
+                            const queryConsulta = ` SELECT * FROM autores WHERE id = ${connection.escape(nuevoId)} `
 
-            connection.query(queryConsulta, function (error, filas, columnas) {
-                res.status(201)
-                res.json({ data: filas[0] })
-            })
+                            connection.query(queryConsulta, function (error, filas, columnas) {
+                                res.status(201)
+                                res.json({ data: filas[0] })
+                            })
 
 
+
+                        })
+                     
+                        connection.release()
+                    }
+
+                 
+                 
+                })    
+            }
 
         })
-        connection.release()
+        
 
     })
-
 })
 
 module.exports = router
